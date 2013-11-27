5cc2b9a403f421a5e7002fff6d35ddcedf584154
Lu Zhang <luzhang@jmlvyou.com>
2013-11-27 11:50:33 +0800

    frist commit

---
+++ trunk/cache/setting.php
@@ -0,0 +1,41 @@
+<?php
+if(!$IWEB_SHOP_IN) {die('Hacking attempt');} 
+$SYSINFO['sys_name'] = '桂林旅游网'; 
+$SYSINFO['sys_title'] = 'Guillin GDS'; 
+$SYSINFO['sys_keywords'] = 'Guilin'; 
+$SYSINFO['sys_description'] = 'Gulin'; 
+$SYSINFO['sys_company'] = '桂林旅游局'; 
+$SYSINFO['sys_copyright'] = 'Copyright © 2013'; 
+$SYSINFO['sys_icp'] = '京ICP备09092256号 '; 
+$SYSINFO['sys_countjs'] = ''; 
+$SYSINFO['sys_registerinfo'] = ''; 
+$SYSINFO['sys_kftelphone'] = '0531-'; 
+$SYSINFO['email_send'] = 'false'; 
+$SYSINFO['sys_smtpserver'] = 'mail.qq.com'; 
+$SYSINFO['sys_smtpserverport'] = '25'; 
+$SYSINFO['sys_smtpusermail'] = ''; 
+$SYSINFO['sys_smtpuser'] = 'admin'; 
+$SYSINFO['sys_smtppass'] = 'admin'; 
+$SYSINFO['web'] = 'http://localhost/gds/'; 
+$SYSINFO['lp'] = 'zh'; 
+$SYSINFO['timezone'] = '8'; 
+$SYSINFO['session'] = 'gds_'; 
+$SYSINFO['url_r'] = 'false'; 
+$SYSINFO['sys_domain'] = '0'; 
+$SYSINFO['site_domain'] = '.guilin.com'; 
+$SYSINFO['im_enable'] = 'false'; 
+$SYSINFO['offline'] = 'true'; 
+$SYSINFO['off_info'] = '站点维护中。。。'; 
+$SYSINFO['seller_page'] = '10'; 
+$SYSINFO['search_page'] = '10'; 
+$SYSINFO['product_page'] = '10'; 
+$SYSINFO['article_page'] = '10'; 
+$SYSINFO['height1'] = '84'; 
+$SYSINFO['width1'] = '84'; 
+$SYSINFO['height2'] = '300'; 
+$SYSINFO['width2'] = '300'; 
+$SYSINFO['map'] = 'false'; 
+$SYSINFO['map_key'] = ''; 
+$SYSINFO['sys_logo'] = ''; 
+$SYSINFO['verifycode'] = 'a:4:{i:1;s:1:"0";i:2;s:1:"1";i:3;s:1:"1";i:4;s:1:"0";}'; 
+?>
