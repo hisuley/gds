c4dbc9cb67f46c110bd613b6cd3583db0c77bc4c
Suley <dearsuley@gmail.com>
2014-01-09 07:07:56 +0800

    promote

---
+++ trunk/models/promote.php
@@ -0,0 +1,128 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+$IWEB_SHOP_IN = true;
+
+require("foundation/asession.php");
+require("configuration.php");
+require("includes.php");
+require_once("foundation/fstring.php");
+require("foundation/flefttime.php");
+require("foundation/module_tag.php");
+require("foundation/module_nav.php");
+
+/* 用户信息处理 */
+require 'foundation/alogin_cookie.php';
+if(get_sess_user_id()) {
+	$USER['login'] = 1;
+	$USER['user_name'] = get_sess_user_name();
+	$USER['user_id'] = get_sess_user_id();
+	$USER['user_email'] = get_sess_user_email();
+	$USER['shop_id'] = get_sess_shop_id();
+} else {
+	$USER['login'] = 0;
+	$USER['user_name'] = '';
+	$USER['user_id'] = '';
+	$USER['user_email'] = '';
+	$USER['shop_id'] = '';
+}
+
+
+//引入语言包
+$i_langpackage = new indexlp;
+$s_langpackage=new shoplp;
+
+/* 定义文件表 */
+$t_shop_info = $tablePreStr."shop_info";
+$t_user_info = $tablePreStr."user_info";
+$t_users = $tablePreStr."users";
+$t_shop_category = $tablePreStr."shop_category";
+$t_goods = $tablePreStr."goods";
+$t_promote = $tablePreStr."promotions";
+$t_areas = $tablePreStr."areas";
+$t_tag = $tablePreStr."tag";
+$t_nav = $tablePreStr."nav";
+$t_category = $tablePreStr."category";
+
+
+/* canshu */
+$kk = short_check(get_args("k"));
+/* 数据库操作 */
+dbtarget('r',$dbServs);
+$dbo=new dbex();
+
+/* 产品处理 */
+$sql_best = "SELECT * FROM $t_goods WHERE is_on_sale=1 AND is_best=1 and lock_flg=0 order by pv desc limit 4";
+$sql_hot = "SELECT * FROM $t_goods WHERE is_on_sale=1 AND is_hot=1 and lock_flg=0 order by pv desc limit 10";
+$goods_best = $dbo->getRs($sql_best);
+$goods_hot = $dbo->getRs($sql_hot);
+
+/* 用户资料 */
+
+
+/* 浏览记录 */
+$getcookie = get_hisgoods_cookie();
+$goodshistory = array();
+if($getcookie) {
+	arsort($getcookie);
+	$getcookie = array_keys($getcookie);
+	$gethisgoodsid = implode(",",array_slice($getcookie, 0, 4));
+	$sql = "select is_set_image,goods_id,goods_name,goods_thumb,goods_price from $t_goods where goods_id in ($gethisgoodsid)";
+	$goodshistory = $dbo->getRs($sql);
+}
+
+$header['title'] = "促销 - ".$SYSINFO['sys_title'];
+$header['keywords'] = $i_langpackage->i_lay_out.','.$SYSINFO['sys_keywords'];
+$header['description'] = $SYSINFO['sys_description'];
+
+/* 时间处理 */
+$now_time = new time_class();
+$now_time = $now_time -> short_time();
+
+$promote_id=array();
+$sql="select id from $t_promote where is_enabled ='1'";
+$promotes_id = $dbo->getRs($sql);
+foreach($promotes_id as $key=>$val){
+    $promote_id[$key]=$val[0];
+}
+$promote_id=implode(',',$promote_id);
+
+$sql="update $t_promote set is_enabled ='0' where  start_time <= '$now_time' and '$now_time' <= end_time ";
+if($promote_id){
+	$sql.=" and id in($promote_id)";
+}
+$dbo->exeUpdate($sql);
+
+$promote_id=array();
+$sql="select id from $t_promote where is_enabled ='1'";
+$promotes_id = $dbo->getRs($sql);
+foreach($promotes_id as $key=>$val){
+    $promote_id[$key]=$val[0];
+}
+$promote_id=implode(',',$promote_id);
+
+
+$sql = "SELECT b.*,g.* FROM `$t_promote` b left join `$t_goods` g on b.goods_id = g.goods_id";
+$sql .= " WHERE b.recommended = 0 and g.lock_flg =0 and b.group_condition ='0' and b.is_enabled = '1'";
+if(!empty($kk)){
+    $sql .= " and (g.goods_name LIKE '%".trim($kk)."%' OR b.content LIKE '%".trim($kk)."%') ";
+}
+//$sql .= " and b.start_time <= '$now_time' and '$now_time' <= b.end_time";
+$result = $dbo->fetch_page($sql,$SYSINFO['product_page']);
+$tag_list = get_tag_list($dbo,$t_tag,15);
+
+
+$nav_selected =6;
+$nav_list = get_nav_list($t_nav,$dbo);
+
+	/* 处理系统分类 */
+$sql_category = "select * from `$t_category` order by sort_order asc,cat_id asc,sort_order asc";
+$result_category = $dbo->getRs($sql_category);
+
+$CATEGORY = array();
+if($result_category) {
+	foreach($result_category as $v) {
+		$CATEGORY[$v['parent_id']][$v['cat_id']] = $v;
+
+	}
+}
+?>
