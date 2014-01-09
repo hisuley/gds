4b6faca965c8c1559a863adcc5c7c88a8b080eeb
Suley <dearsuley@gmail.com>
2014-01-09 08:56:40 +0800

    增加返回按钮

--- trunk/models/category.php
+++ trunk/models/category.php
@@ -99,7 +99,7 @@ if(isset($attr_arr)&&$attr_arr){
 if($this_catinfo['cat_name'] == '景区' || $this_catinfo['cat_name'] == '景点'){
     $jingqu_sql_hot = "SELECT * FROM $t_goods WHERE is_on_sale=1 AND cat_id = ".$cat_id." AND  lock_flg=0 order by pv desc limit 5";
     $jingqu_goods_hot = $dbo->getRs($jingqu_sql_hot);
-    $jingqu_sql_gonglue = "SELECT * FROM $t_article WHERE id = 21 LIMIT 5";
+    $jingqu_sql_gonglue = "SELECT * FROM $t_article WHERE cat_id = 21 LIMIT 5";
     $jingqu_gonglue = $dbo->getRs($jingqu_sql_gonglue);
 }
 $get_goods_id = "SELECT id FROM $t_goods WHERE is_on_sale = 1 AND cat_id = ".$cat_id." AND  lock_flg=0 order by pv desc limit 5";
