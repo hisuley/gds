8693a4c513d9e632c6067545733255b4ba5e2fb1
Suley <dearsuley@gmail.com>
2014-01-09 09:53:37 +0800

    fix 评论

--- trunk/action/goods/goods_credit.php
+++ trunk/action/goods/goods_credit.php
@@ -23,7 +23,7 @@ dbtarget('r' , $dbServs);
 $dbo = new dbex;
 
 //查询商品评价
-$sql = "select c.cid,c. buyer_evaltime, c.buyer_evaluate,c.buyer_credit,u.user_id,u.user_name from `$t_credit` as c join `$t_users` as u on c.buyer=u.user_id where c.goods_id = $goods_id and  c.buyer_evaltime is not null ";
+$sql = "select c.cid,c. buyer_evaltime, c.buyer_evaluate,c.buyer_credit,u.user_id,u.user_name from `$t_credit` as c join `$t_users` as u on c.buyer=u.user_id where c.goods_id = $goods_id";
 $result = $dbo->fetch_page($sql,10);
 
 //返回json数据
