92babd337b26c62c582de94f2d8ae266e16905a9
Suley <dearsuley@gmail.com>
2014-01-11 16:53:37 +0800

    DB配置

--- trunk/action/ajax/mms.action.php
+++ trunk/action/ajax/mms.action.php
@@ -8,22 +8,12 @@
 
 
 require("foundation/nusoap/nusoap.php");
+require('foundation/module_category.php');
 
-$url="http://121.52.221.108/websend/MmsService?WSDL";
-$type= "WSDL";
-$charset="utf-8";
-$param=array(
-    'sName'=> "guilin",//手机号
-    'sPwd'=> "123456",
-    'start'=> "0", //用户名
-    'end'=> "1"
-);
-
-$client = new nusoap_client($url,$type);
-if($err = $client->getError()) exit($err);
-$client->soap_defencoding = $charset;
-$client->decode_utf8 = false;
-$client->xml_encoding = $charset;
-$result = $client->call("GetMmsList",$param,'','',false,true);
+dbtarget('r',$dbServs);
+$dbo=new dbex();
+$table = $tablePreStr."category";
+$result = get_sub_under($dbo, $table, '433');
 print_r($result);
+
 ?>
--- trunk/dms_mini_lib/conf/dbconf.php
+++ trunk/dms_mini_lib/conf/dbconf.php
@@ -3,13 +3,14 @@ if(!$IWEB_SHOP_IN) {
 	die("Hacking attempt");
 }
 $host = 'localhost';//mysql数据库服务器,比如localhost:3306
-$user = 'root'; //mysql数据库默认用户名
-$pwd = '123123'; //mysql数据库默认密码
-$db = 'gds'; //默认数据库名
+$user = 'dms'; //mysql数据库默认用户名
+$pwd = '8a7316fb3f96f4e3b20249e7a05dd0f92bddc81c'; //mysql数据库默认密码
+$db = 'dms_v01'; //默认数据库名
 global $tablePreStr;
 $tablePreStr = 'imall_';//表前缀
 
 //当前提供服务的mysql数据库
 global $dbServs;
 $dbServs=array($host,$db,$user,$pwd);
+
 ?>
